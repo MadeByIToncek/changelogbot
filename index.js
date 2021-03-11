73f9598b9d77c5cd3219a3dfcd5817b79061612c
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2021-03-11 11:59:23 +0100

    nahrani blank wokcommands

---
+++ index.js
@@ -0,0 +1,39 @@
+const DiscordJS = require('discord.js')
+const WOKCommands = require('wokcommands')
+require('dotenv').config()
+
+const client = new DiscordJS.Client({
+  partials: ['MESSAGE', 'REACTION'],
+})
+
+client.on('ready', () => {
+  // See the "Language Support" section of this documentation
+  // An empty string = ignored
+  const messagesPath = ''
+
+  // If you want to disable built in commands you can add them to this array. Simply uncomment the strings to disable that command.
+
+  const disabledDefaultCommands = [
+     'help',
+     'command',
+     'language',
+     'prefix',
+     'requiredrole'
+  ]
+
+  // Initialize WOKCommands with specific folders and MongoDB
+  new WOKCommands(client, {
+    commandsDir: 'commands',
+    featureDir: 'features',
+    messagesPath,
+    showWarns: false, // Show start up warnings
+    disabledDefaultCommands
+  })
+    // Set your MongoDB connection path
+    // Set the default prefix for your bot, it is ! by default
+    .setDefaultPrefix(process.env.PREFIX)
+    // Set the embed color for your bot. The default help menu will use this. This hex value can be a string too
+    .setColor(0xff0000)
+},
+
+client.login(process.env.TOKEN))
